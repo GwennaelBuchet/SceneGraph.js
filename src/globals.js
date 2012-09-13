44d7535d496a4dcb95805c008d5c620d9158e35c
Gwennael Buchet <gwennael.buchet@capgemini.com>
2012-09-13 10:31:44 +0200

    increase version (1.0.1)

--- src/globals.js
+++ src/globals.js
@@ -29,7 +29,7 @@
  * global parameters for the application
  */
 //current version of the framework
-var cgsgVersion = "0.9.8.0";
+var cgsgVersion = "1.0.1";
 
 //current display ratio
 var cgsgDisplayRatio = CGSG_DEFAULT_DISPLAYRATIO;
