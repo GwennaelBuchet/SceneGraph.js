e7e3ae22c1a0de5b097266795bbc153bf9e3b5e6
Gwennael Buchet <gwennael.buchet@capgemini.com>
2012-12-07 17:52:26 +0100

    - Evolution. Add CGSGColor "rgb2hsl", "hsl2rgb", "multiplyComponent", "darkenHex" and "darkenRGB" method - Evolution. Add of CGSGNodeColorPicker - Evolution. Add of CGSGNodeTabMenu

---
+++ src/node/class.node.colorPicker.js
@@ -0,0 +1,258 @@
+/*
+ * Copyright (c) 2012  Capgemini Technology Services (hereinafter “Capgemini”)
+ *
+ * License/Terms of Use
+ *
+ * Permission is hereby granted, free of charge and for the term of intellectual property rights on the Software, to any
+ * person obtaining a copy of this software and associated documentation files (the "Software"), to use, copy, modify
+ * and propagate free of charge, anywhere in the world, all or part of the Software subject to the following mandatory conditions:
+ *
+ *   •    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *  Any failure to comply with the above shall automatically terminate the license and be construed as a breach of these
+ *  Terms of Use causing significant harm to Capgemini.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *  Except as contained in this notice, the name of Capgemini shall not be used in advertising or otherwise to promote
+ *  the use or other dealings in this Software without prior written authorization from Capgemini.
+ *
+ *  These Terms of Use are subject to French law.
+ */
+
+var CGSG_COLORPICKER_WIDTH = 256;
+var CGSG_COLORPICKER_HEIGHT = 384;
+
+/**
+ * A color Picker
+ * @class CGSGNodeColorPicker
+ * @constructor
+ * @param {Number} x X Position
+ * @param {Number} y Y Position
+ * @type {CGSGNodeColorPicker}
+ */
+var CGSGNodeColorPicker = CGSGNode.extend(
+    {
+        initialize:function (x, y) {
+            this._super(x, y, CGSG_COLORPICKER_WIDTH, CGSG_COLORPICKER_HEIGHT);
+
+            /**
+             * @property _imgData
+             * @type {ImageData}
+             * @private
+             */
+            this._imgData = null;
+
+            //fake canvas to pre-render static display
+            this._tmpCanvas = null;
+            this._initShape();
+
+            var that = this;
+            this.onMouseOver = function (event) {
+                that._onMouseOverHandler(event);
+            };
+            this.onClick = function (event) {
+                that._onClickHandler(event);
+            };
+
+            /**
+             * Event fired when the mice cursor move over the color picker. Return a {r, g, b} Object.
+             * @property onOverColor
+             * @default null
+             * @type {Function}
+             */
+            this.onOverColor = null;
+            /**
+             * Event fired when the user click on the color picker. Return a {r, g, b} Object.
+             * @property onClickColor
+             * @default null
+             * @type {Function}
+             */
+            this.onClickColor = null;
+        },
+
+        /**
+         * Pre-render the node into a temp canvas to optimize the perfs
+         * @method _initShape
+         * @private
+         */
+        _initShape:function () {
+            this._tmpCanvas = document.createElement('canvas');
+            this._tmpCanvas.width = this.dimension.width + 2;
+            this._tmpCanvas.height = this.dimension.height + 2;
+            var tmpContext = this._tmpCanvas.getContext('2d');
+
+            this._imgData = tmpContext.createImageData(this.getWidth(), this.getHeight());
+
+            //draw the colors panel
+            var x, y, intensity = 1, stepX, currentPixel = 0;
+            var rgb = [255, 0, 0], clr = 0, delta, value, tmpClr;
+            var widthGray = CGSGMath.fixedPoint(Math.min(this.getWidth() * 0.1, 20));
+            var width = this.getWidth() - widthGray;
+            var steps = [
+                {index:1, sens:1},
+                {index:0, sens:-1},
+                {index:2, sens:1},
+                {index:1, sens:-1},
+                {index:0, sens:1},
+                {index:2, sens:-1}
+            ];
+            //A color is divided in 256 values (from 0 to 255).
+            //ALl these 256 values won't be displayed because a lack of space in the screen (limited to this.getWidth().
+            //Also, there are 6 steps in the colorpicker : red, yellow, green, cyan, blue, violet
+            // So, stepX is the step between 2 computed values, depending of the width of the node
+            stepX = 256 / (width / 6);
+            var halfH = this.getHeight() / 2;
+
+            //the white to black column
+            for (x=0; x<widthGray; x++) {
+                for (y = 0; y < this.getHeight(); y++) {
+                    //current pixel in the linear table
+                    currentPixel = (y * this.getWidth() + x) * 4; // 4 because 4 values per pixel : RGBA
+
+                    intensity = 1 - (y / this.getHeight());
+
+                    this._imgData.data[currentPixel + 0] = 255 * intensity;
+                    this._imgData.data[currentPixel + 1] = 255 * intensity;
+                    this._imgData.data[currentPixel + 2] = 255 * intensity;
+                    this._imgData.data[currentPixel + 3] = 255;
+                }
+            }
+
+            //the colors columns
+            for (x = widthGray; x < this.getWidth(); x++) {
+                intensity = 1;
+                for (y = 0; y < this.getHeight(); y++) {
+                    //current pixel in the linear table
+                    currentPixel = (y * this.getWidth() + x) * 4; // 4 because 4 values per pixel : RGBA
+
+                    //from white to current color
+                    if (y < halfH) {
+                        intensity = 2 - (y / halfH);
+                        tmpClr = CGSGColor.litRGB(rgb[0], rgb[1], rgb[2], intensity);
+                    }
+                    //from current color to black
+                    else {
+                        intensity = 1 - ((y - halfH) / halfH);
+                        tmpClr = CGSGColor.darkenRGB(rgb[0], rgb[1], rgb[2], intensity);
+                    }
+
+                    this._imgData.data[currentPixel + 0] = tmpClr.r;
+                    this._imgData.data[currentPixel + 1] = tmpClr.g;
+                    this._imgData.data[currentPixel + 2] = tmpClr.b;
+                    this._imgData.data[currentPixel + 3] = 255;
+                }
+                delta = stepX * steps[clr].sens;
+                value = rgb[steps[clr].index] + delta;
+                rgb[steps[clr].index] = value;
+                if (value <= 0 || value >= 255) {
+                    rgb[steps[clr].index] = Math.min(Math.max(value, 0), 255);
+                    clr++;
+                }
+            }
+
+            tmpContext.putImageData(this._imgData, 0, 0);
+        },
+
+        /**
+         * Override the parent method.
+         * @method resizeTo
+         * @param {Number} x
+         * @param {Number} y
+         */
+        resizeTo:function (x, y) {
+            this._super(x, y);
+            this._initShape();
+        },
+
+        /**
+         * Override the parent method.
+         * @method resizeWith
+         * @param {Number} w
+         * @param {Number} h
+         */
+        resizeWith:function (w, h) {
+            this._super(w, h);
+            this._initShape();
+        },
+
+        /**
+         * Override the parent method.
+         * @method resizeBy
+         * @param {Number} w
+         * @param {Number} h
+         */
+        resizeBy:function (w, h) {
+            this._super(w, h);
+            this._initShape();
+        },
+
+        /**
+         * @method _onMouseOverHandler
+         * @param {Event} event
+         * @private
+         */
+        _onMouseOverHandler:function (event) {
+            if (cgsgExist(this.onOverColor)) {
+                var rgb = this.getColorAt(event.position[0]);
+
+                this.onOverColor(rgb);
+            }
+        },
+
+        /**
+         * @method _onClickHandler
+         * @param {Event} event
+         * @private
+         */
+        _onClickHandler:function (event) {
+            if (cgsgExist(this.onClickColor)) {
+                var rgb = this.getColorAt(event.position[0]);
+
+                this.onClickColor(rgb);
+            }
+        },
+
+        /**
+         * @method getColorAt
+         * @param {CGSGPosition} relativePosition position of the cursor inside the colorPicker
+         * @return {Object} Object with {r:x, g:x, b:x} value
+         */
+        getColorAt:function (relativePosition) {
+            var ap = this.getAbsolutePosition();
+            var aw = this.getAbsoluteWidth();
+            //get the color under the mice
+            var data = this._imgData.data;
+            //get cursor position under the colorPicker
+            var x = relativePosition.x - ap.x;
+            var y = relativePosition.y - ap.y;
+
+            return {r:data[((aw * y) + x) * 4],
+                g:data[((aw * y) + x) * 4 + 1],
+                b:data[((aw * y) + x) * 4 + 2]};
+        },
+
+        /**
+         * Custom rendering. Must be defined to allow the traverser to render this node
+         * @method render
+         * @protected
+         * @param {CanvasRenderingContext2D} context the context into render the node
+         * */
+        render:function (context) {
+            //call this before your custom rendering
+            this.beforeRender(context);
+
+            context.globalAlpha = this.globalAlpha;
+
+            //render the pre-rendered canvas
+            context.drawImage(this._tmpCanvas, 0, 0);
+
+            //call this after your custom rendering
+            this.afterRender(context);
+        }
+
+    }
+);
--- src/utils/util.color.js
+++ src/utils/util.color.js
@@ -103,18 +103,203 @@ var CGSGColor = {
 
     /**
      *
-     * @method lit
+     * @method darkenHex
      * @param {String} hex Hexadecimal value of a color (with or without the started '#'
-     * @param {Number} ratio If >0 : lighten. If <0 : darken
+     * @param {Number} factor If >0 : lighten. If <0 : darken
      * @return {String}
      */
-    lit:function (hex, ratio) {
+    darkenHex:function (hex, factor) {
         var rgb = this.hex2rgb(hex);
 
-        rgb.r = Math.max(255, rgb.r * ratio);
-        rgb.g = Math.max(255, rgb.g * ratio);
-        rgb.b = Math.max(255, rgb.b * ratio);
+        rgb.r = this.multiplyComponent(rgb.r, factor);
+        rgb.g = this.multiplyComponent(rgb.g, factor);
+        rgb.b = this.multiplyComponent(rgb.b, factor);
 
         return this.rgb2hex(rgb.r, rgb.g, rgb.b);
+    },
+
+    /**
+     *
+     * @method darkenRGB
+     * @param {Number} r Red value between 0 and 255
+     * @param {Number} g Green value between 0 and 255
+     * @param {Number} b Blue value between 0 and 255
+     * @param {Number} factor If >0 : lighten. If <0 : darken
+     * @return {Object} An {r, g, b} object
+     */
+    darkenRGB:function (r, g, b, factor) {
+        return {
+            r:this.multiplyComponent(r, factor),
+            g:this.multiplyComponent(g, factor),
+            b:this.multiplyComponent(b, factor)
+        };
+    },
+
+    litRGB:function (r, g, b, factor) {
+        var hsl = this.rgb2hsl(r, g, b);
+
+        hsl.l *= factor;
+
+        return this.hsl2rgb(hsl.h, hsl.s, hsl.l);
+    },
+
+    /**
+     * @method multiplyComponent
+     * @param {Number} c color component between 0 and 25(
+     * @param {Number} factor
+     * @return {Number} The multiplied value, between 0 and 255
+     */
+    multiplyComponent:function (c, factor) {
+        return Math.max(Math.min(255, c * factor), 0);
+    },
+
+    /**
+     * RGB to HSV converter.
+     * Adapted from http://en.wikipedia.org/wiki/HSV_color_space
+     * @method rgb2hsv
+     * @param {Number} r
+     * @param {Number} g
+     * @param {Number} b
+     * @return {Object} An {h, s, v} object
+     */
+    rgb2hsv:function (r, g, b) {
+        var min, max;
+
+        //normalize the 3 components
+        r /= 255;
+        g /= 255;
+        b /= 255;
+        min = Math.min(b, Math.min(r, g));
+        max = Math.max(b, Math.max(r, g));
+
+        //min == max ? so the 3 values are the same, we got a gray color.
+        //just return the lightness
+        if (max == min) {
+            return {h:0, s:0, v:min};
+        }
+
+        var d = (r == min) ? g - b : ((b == min) ? r - g : b - r);
+        var h = (r == min) ? 3 : ((b == min) ? 1 : 5);
+
+        var dM = max - min;
+        return {
+            h:60 * (h - d / dM),
+            s:dM / max,
+            v:max
+        };
+    },
+
+    /**
+     * RGB to HSL converter.
+     * Adapted from http://en.wikipedia.org/wiki/HSL_color_space
+     * @method rgb2hsl
+     * @param {Number} r
+     * @param {Number} g
+     * @param {Number} b
+     * @return {Object}
+     */
+    rgb2hsl:function (r, g, b) {
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if (max == min) {
+            h = s = 0;
+        } else {
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch (max) {
+                case r:
+                    h = (g - b) / d + (g < b ? 6 : 0);
+                    break;
+                case g:
+                    h = (b - r) / d + 2;
+                    break;
+                case b:
+                    h = (r - g) / d + 4;
+                    break;
+            }
+            h /= 6;
+        }
+
+        return {h:h, s:s, l:l};
+    },
+
+    /**
+     * HSL to RGB converter.
+     * Adapted from http://en.wikipedia.org/wiki/HSL_color_space
+     * @method hsl2rgb
+     * @param {Number} h The hue
+     * @param {Number} s The saturation
+     * @param {Number} l the lightness
+     * @return {Object} A {r, g, b} object
+     */
+    hsl2rgb:function (h, s, l) {
+        var r, g, b;
+
+        if (s == 0) {
+            r = g = b = l;
+        } else {
+            function hue2rgb(p, q, t) {
+                if (t < 0) t += 1;
+                if (t > 1) t -= 1;
+                if (t < 1 / 6) return p + (q - p) * 6 * t;
+                if (t < 1 / 2) return q;
+                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1 / 3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1 / 3);
+        }
+
+        return {r:r * 255, g:g * 255, b:b * 255};
+    },
+
+
+    /**
+     * HSV to RGB converter.
+     * Adapted from http://en.wikipedia.org/wiki/HSV_color_space
+     * @method hsv2rgb
+     * @param {Number} h The hue
+     * @param {Number} s The saturation
+     * @param {Number} v the value
+     * @return {Object} A {r, g, b} object
+     */
+    hsv2rgb:function (h, s, v) {
+        var r, g, b;
+
+        var i = Math.floor(h * 6);
+        var f = h * 6 - i;
+        var p = v * (1 - s);
+        var q = v * (1 - f * s);
+        var t = v * (1 - (1 - f) * s);
+
+        switch (i % 6) {
+            case 0:
+                r = v, g = t, b = p;
+                break;
+            case 1:
+                r = q, g = v, b = p;
+                break;
+            case 2:
+                r = p, g = v, b = t;
+                break;
+            case 3:
+                r = p, g = q, b = v;
+                break;
+            case 4:
+                r = t, g = p, b = v;
+                break;
+            case 5:
+                r = v, g = p, b = q;
+                break;
+        }
+
+        return {r:r * 255, g:g * 255, b:b * 255};
     }
+
 };
