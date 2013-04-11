0e34449089c75a10f734acbc620783630c499bd2
Gwennael Buchet <gwennael.buchet@gmail.com>
2013-04-11 18:12:28 +0200

    Add check box  and spinner examples on Ecosystem page

---
+++ community/jvanl/checkbox/js/class.main.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2012  Capgemini Technology Services (hereinafter “Capgemini”)
+ *
+ * License/Terms of Use
+ *
+ * Permission is hereby granted, free of charge and for the term of intellectual property rights on the Software, to any
+ * person obtaining a copy of this software and associated documentation files (the "Software"), to use, copy, modify
+ * and propagate free of charge, anywhere in the world, all or part of the Software subject to the following mandatory conditions:
+ *
+ *   •    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *  Any failure to comply with the above shall automatically terminate the license and be construed as a breach of these
+ *  Terms of Use causing significant harm to Capgemini.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *  Except as contained in this notice, the name of Capgemini shall not be used in advertising or otherwise to promote
+ *  the use or other dealings in this Software without prior written authorization from Capgemini.
+ *
+ *  These Terms of Use are subject to French law.
+ *
+ * @author Gwennael Buchet (gwennael.buchet@capgemini.com)
+ * @date 10/08/2012
+ *
+ * Purpose :
+ * Template project
+ * */var CGMain = CGSGScene.extend(
+	{
+		initialize: function (canvas) {
+
+			this._super(canvas);
+
+			////// INITIALIZATION /////////
+
+			this.initializeCanvas();
+
+			this.squareNode = null;
+
+			this.createScene();
+
+			this.startPlaying();
+		},
+
+		initializeCanvas: function () {
+			//redimensionnement du canvas pour être full viewport en largeur
+			this.viewDimension = cgsgGetRealViewportDimension();
+			this.setCanvasDimension(this.viewDimension);
+		},
+
+		/**
+		 * Just create a single node (a square node)
+		 *
+		 */
+		createScene: function () {
+
+            var chkboxgroup = new CGSGCheckboxGroupNode(100, 100, 100, 100);
+            chkboxgroup.addCheckboxButton("test", "value");
+            chkboxgroup.addCheckboxButton("test2", "value2");
+            chkboxgroup.addCheckboxButton("test3", "value3");
+            chkboxgroup.addCheckboxButton("test4", "value4");
+
+            // display selection information
+            var yourChoice = new CGSGNodeText(200, 50, "Your selection");
+
+            chkboxgroup.onSelect = function() {
+                var text = "Your selection : ";
+                var selections = chkboxgroup.getCurrentSelection();
+
+                for(var i=0; i<selections.length; i++) {
+                    text += "[ text : " + selections[i].key + ", value : " + selections[i].value + " ]";
+                }
+
+                yourChoice.setText(text);
+
+            };
+
+
+            this.sceneGraph.addNode(chkboxgroup, null);
+            this.sceneGraph.addNode(yourChoice, null);
+		}
+
+	}
+);
