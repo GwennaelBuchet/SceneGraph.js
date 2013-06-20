1e02e05eab7b496d2a2832b109de253d87751f03
Nathan Damie <nathan.damie@gmail.com>
2013-06-20 21:33:18 +0200

    new node CGSGAccordion

---
+++ src/node/class.node.accordion.js
@@ -0,0 +1,207 @@
+/**
+ * This class represents an Accordion.
+ * @class CGSGAccordion
+ * @module Node
+ * @extends CGSGNode
+ * @constructor
+ * @param {Number} x Relative position on X
+ * @param {Number} y Relative position on Y
+ * @param {Number} width Relative dimension
+ * @param {Number} height Relative Dimension
+ * @param {Number} sectionWidth Relative Dimension
+ * @param {Number} sectionHeight Relative Dimension
+ * @type {CGSGAccordion}
+ * @author $Author$
+ */
+var CGSGAccordion = CGSGNode.extend(
+    {
+        initialize : function(x, y, width, height, sectionWidth, sectionHeight) {
+            this._super(x, y, width, height);
+
+            this.lineWidth = sectionWidth;
+            this.lineHeight = sectionHeight;
+            this.interLine = 2;
+            this.padding = 5;
+            this.speed = 10;
+
+            this.sections = [];
+
+            /**
+             * Event
+             * @property onSectionChanged
+             * @default null
+             * @type {Function}
+             */
+            this.onSectionChanged = null;
+
+        },
+
+        /**
+         * Add a new section on the accordion.
+         * @param {CGSGNode} title
+         * @param {CGSGNode} core
+         */
+        buildAndAddSection : function(title, core) {
+            var section = new CGSGSection(0, 0, this.lineWidth, this.lineHeight);
+            //add the title to the section.
+            section.setTitle(title);
+
+            //add the core to the section
+            section.setCore(core);
+
+            //place the section at the end of the accordion
+            section.translateTo(0,  this.sections.length * (this.lineHeight + this.interLine) );
+            section.index = this.sections.length;
+            this.sections.push(section);
+
+            //bind selection onClick
+            var that = this;
+            section.onClick = function(e) {
+                that.selectSection(e.data.node);
+            };
+
+            this.addChild(section);
+            return section;
+        },
+
+        /**
+         * remove a section from the accordion.
+         * @param {Number} index
+         */
+        removeSectionAt : function (index) {
+
+            var section = this.sections.splice(index, 1);
+
+            this.removeChild(section[0], true);
+
+            //replace all element
+            this.deselectAll();
+
+            //return removed section
+            return section;
+        },
+
+        /**
+         * Display the core of the selected section.
+         * @param {CGSGNode} section
+         */
+        selectSection : function (section) {
+            var open = section.isOpen == true ? false : true;
+            this.deselectAll();
+
+            if (open) {
+                //animate the selection
+              CGSG.animationManager.animate(section, "dimension.height", this.speed, section.getHeight(), section.getHeight() + section.core.getHeight() + this.padding, 0);
+              CGSG.animationManager.animate(section, "mask._maskRegion.dimension.height", this.speed, this.lineHeight, this.lineHeight + section.core.getHeight() + this.padding , 0);
+
+                //translate other section
+                $.each(this.sections, $.proxy(function(i, sec) {
+                    if(section.index < sec.index) {
+                        CGSG.animationManager.animate(sec, "position.y", this.speed, sec.position.y, section.core.getHeight() + this.padding + sec.index * (this.lineHeight + this.interLine), 0);
+                    }
+                }, this));
+            }
+            section.isOpen = open;
+        },
+
+        /**
+         * deselect all the section.
+         */
+        deselectAll : function () {
+                $.each(this.sections, $.proxy(function(i, section) {
+                           CGSG.animationManager.animate(section, "position.y", this.speed, section.position.y, i * (this.lineHeight + this.interLine) , 0);
+                        if(section.isOpen) {
+                            CGSG.animationManager.animate(section, "mask._maskRegion.dimension.height", this.speed, section.mask._maskRegion.dimension.height , this.lineHeight, 0);
+                            CGSG.animationManager.animate(section, "dimension.height", this.speed, section.getHeight(), this.lineHeight, 0);
+                            section.isOpen = false;
+                        }
+                }, this));
+        }
+    }
+);
+
+/**
+ * This class represents a Accordion.
+ * @class CGSGSection
+ * @module Node
+ * @extends CGSGNode
+ * @constructor
+ * @param {Number} x Relative position on X
+ * @param {Number} y Relative position on Y
+ * @param {Number} width Relative dimension
+ * @param {Number} height Relative Dimension
+ * @author $Author$
+ */
+var CGSGSection = CGSGNode.extend(
+    {
+        initialize : function (x, y, width, height) {
+            this._super(x, y);
+            this.resizeTo(width, height);
+
+            this._backgroundColor = "#605C55";
+            this.lineHeight = this.getHeight();
+            this.isOpen = false;
+            this.index = null;
+            this.mask = new CGSGMaskClip(new CGSGRegion(0, 0,this.getWidth(), this.getHeight()));
+            this.mask.apply(this);
+
+
+        },
+
+        /**
+         * set the title of the section header
+         * @param {CGSGNode} title
+         */
+        setTitle : function (title) {
+            this.title = title;
+            this.title.translateTo(this.getHeight(),( this.getHeight() - this.title.getHeight() ) / 2 )
+            this.addChildAt(title, 0);
+        },
+
+
+        /**
+         * set the core of the section
+         * @param {CGSGNode} core
+         */
+        setCore : function (core) {
+            this.core = core;
+            this.addChildAt(this.core, 1);
+            this.core.translateTo(( this.getWidth() - this.core.getWidth() ) / 2,  this.getHeight());
+        },
+
+        /**
+         * change the background color of the section
+         * @param color
+         */
+        setBackgroundColor : function (color) {
+            this._backgroundColor = color;
+        },
+
+        render : function (ctx) {
+            ctx.beginPath();
+            ctx.fillStyle = this._backgroundColor;
+            ctx.globalAlpha = "0.8";
+
+            //display background
+            ctx.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            //display icon
+            ctx.fillStyle = "white";
+            ctx.moveTo(this.lineHeight / 3, this.lineHeight / 3);
+            if (this.isOpen) {
+                ctx.lineTo(2 * this.lineHeight / 3, this.lineHeight / 3);
+                ctx.lineTo(this.lineHeight / 2, 2 * this.lineHeight / 3);
+                ctx.lineTo(this.lineHeight / 3, this.lineHeight / 3);
+            }
+            else {
+                ctx.lineTo(2 * this.lineHeight / 3, this.lineHeight / 2);
+                ctx.lineTo(this.lineHeight / 3, 2 * this.lineHeight / 3);
+                ctx.lineTo(this.lineHeight / 3, this.lineHeight / 3);
+            }
+            ctx.fill();
+
+            ctx.closePath();
+        }
+    }
+);
+
