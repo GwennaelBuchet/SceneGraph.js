d5a5da7277b5b82aadcf7988008c60dce6b5a105
Gwennael Buchet <gwennael.buchet@gmail.com>
2014-04-16 10:28:24 +0200

    Update examples content

--- v2/js/controller/examplesCtrl.js
+++ v2/js/controller/examplesCtrl.js
@@ -1,7 +1,7 @@
-app.controller('ExamplesCtrl', function($scope) {
+app.controller('examplesCtrl', function($scope) {
 	$scope.examples = [
 		{'name' : 'Text node', 'comment' : '...', 'type' : 'basic official', 'link' : '01_Basic_Shapes/01_Text_01_Simple'},
 		{'name' : 'Square node', 'comment' : '...', 'type' : 'basic official', 'link' : '01_Basic_Shapes/02_Square'},
-		{'name' : 'Circle node', 'comment' : '...', 'type' : 'basic official', 'link' : '01_Basic_Shapes/03_Circle'},
+		{'name' : 'Circle node', 'comment' : '...', 'type' : 'basic official', 'link' : '01_Basic_Shapes/03_Circle'}
 	];
 })
