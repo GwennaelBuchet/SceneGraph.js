f2ff62faf4169f867cd0e44f2f64d59d62833362
Gwennael Buchet <gwennael.buchet@gmail.com>
2014-04-16 22:20:31 +0200

    Update Example Controller

--- v2/js/controller/exampleCtrl.js
+++ v2/js/controller/exampleCtrl.js
@@ -1,9 +1,21 @@
-app.controller('exampleCtrl', ['$scope', '$http', '$location', 'ExamplesSrv', function($scope, $http, $location, examplesSrv) {
+/*app.controller('exampleCtrl', ['$scope', '$http', '$location', 'ExamplesSrv', function($scope, $http, $location, examplesSrv) {
 	var searchObject = $location.search();
 
 	examplesSrv.load($scope, $http, loadExample);
 
 	function loadExample() {
+		$scope.example = examplesSrv.find(searchObject.e);
+	}
+
+}]);*/
+
+app.controller('exampleCtrl', ['$scope', '$http', '$location', 'ExamplesSrv', function($scope, $http, $location, examplesSrv) {
+	var searchObject = $location.search();
+	console.log(searchObject);
+	examplesSrv.load($scope, $http, loadExample);
+
+	function loadExample() {
+		//$scope.example = $scope.examples[0];
 		$scope.example = examplesSrv.find(searchObject.e);
 	}
 
