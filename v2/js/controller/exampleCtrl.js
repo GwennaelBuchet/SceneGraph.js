de862265c51fa1c22f4abf4c179e020f91e0af6c
Gwennael Buchet <gwennael.buchet@gmail.com>
2014-04-17 22:06:47 +0200

    Update Example Controller

--- v2/js/controller/exampleCtrl.js
+++ v2/js/controller/exampleCtrl.js
@@ -5,12 +5,15 @@ app.controller(
 	 '$location',
 	 'ExamplesSrv',
 	 'require',
-	 function($scope, $http, $location, examplesSrv, require) {
+	 function ($scope, $http, $location, examplesSrv, require) {
 
 		 var target = examplesSrv.getURLParameter("e");
 		 $scope.require = require;
 
 		 examplesSrv.load($scope, $http, loadExample);
+		 var canvasElt = null;
+		 var sample = null;
+		 var _viewDimension = null;
 
 		 function loadExample() {
 			 $scope.example = examplesSrv.find(target);
@@ -24,12 +27,30 @@ app.controller(
 		 function loadScript(file) {
 			 $scope.require(
 				 [ file ],
-				 function() {
+				 function () {
+					 canvasElt = document.getElementById('divScene');
+
 					 //todo: intialize scene
 					 var canvasScene = document.getElementById("scene");
-					 new CGMain(canvasScene);
+					 sample = new CGMain(canvasScene);
+
+					 //add an handler on the window resize event
+					 window.onresize = resizeCanvas;
+
+					 resizeCanvas();
 				 }
 			 );
+		 }
+
+		 function resizeCanvas() {
+			 _viewDimension = new CGSGDimension(canvasElt.offsetWidth, canvasElt.offsetHeight);
+			 sample.setCanvasDimension(_viewDimension);
+
+			 var sw = _viewDimension.width / 1024;
+			 var sh = _viewDimension.height / 480;
+
+			 var displayRatio = new CGSGScale(sw, sh);
+			 sample.setDisplayRatio(displayRatio);
 		 }
 	 }
 	]
