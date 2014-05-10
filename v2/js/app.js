f3166bda8be6bb4489d83acb9e7c5931ab2f4a2b
Gwennael Buchet <gwennael.buchet@gmail.com>
2014-05-10 14:03:01 +0200

    Use of bound attributes for project description via AngularJS

--- v2/js/app.js
+++ v2/js/app.js
@@ -1,7 +1,16 @@
-var app = angular.module('cgsgApp',[/*'ngLoadScript'*/]);
+var app = angular.module('cgsgApp', [/*'ngLoadScript'*/]);
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
-	$scope.currentversion = "v2.1.0";
+	$scope.project = {
+		"name":"SceneGraph.js",
+		"currentVersion": "v2.1.0",
+		"github":"https://github.com/GwennaelBuchet/cgSceneGraph",
+		"contact":{
+			"firstName":"Gwennaël",
+			"lastName":"Buchet",
+			"email":"gwennael.buchet@gmail.com"
+		}
+	};
 }]);
 
 
