54d3731cb237f7611dd92c3cf32d91da8e9fcaf3
Gwennael Buchet <gwennael.buchet@gmail.com>
2014-05-14 18:05:22 +0200

    Add ecosystemCtrl controller and page

--- v2/js/app.js
+++ v2/js/app.js
@@ -1,15 +1,21 @@
 var app = angular.module('cgsgApp', [/*'ngLoadScript'*/]);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
 	$scope.project = {
-		"name":"SceneGraph.js",
-		"currentVersion": "v2.1.0",
-		"github":"https://github.com/GwennaelBuchet/cgSceneGraph",
-		"contact":{
-			"firstName":"Gwennaël",
-			"lastName":"Buchet",
-			"email":"gwennael.buchet@gmail.com"
+		"name"           : "SceneGraph.js",
+		"currentVersion" : "v2.1.0",
+		"github"         : "https://github.com/GwennaelBuchet/cgSceneGraph",
+		"contact"        : {
+			"firstName" : "Gwennaël",
+			"lastName"  : "Buchet",
+			"email"     : "gwennael.buchet@gmail.com"
 		}
+	};
+
+	$scope.isActive = function(route) {
+		var host = $location.protocol() + "://" + $location.host() + "/cgSceneGraph/";
+		var url = $location.absUrl();
+		return url.substr(host.length, url.length) == route;
 	};
 }]);
 
